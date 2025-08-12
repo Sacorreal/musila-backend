080d9fcbc9f040e697ecee33dac6b27aeb71276f
AgusBlanco <agustinanahirblanco@gmail.com>
2025-08-12 20:27:40 -0300

    feat: [MUS-44-crear-entidades] creación de entidades

--- src/playlists/entities/playlist.entity.ts
+++ src/playlists/entities/playlist.entity.ts
@@ -1,7 +1,43 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Guest } from 'src/guests/entities/guest.entity';
+import { Track } from 'src/tracks/entities/track.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
+@Entity({ name: 'playlist' })
 @ObjectType()
 export class Playlist {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string
+
+  @Column({ type: 'varchar' })
+  @Field()
+  title: string
+
+  @Field(() => User)
+  @ManyToOne(() => User, user => user.playlists)
+  owner: User
+
+  @Column({ type: 'varchar' })
+  @Field()
+  cover?: string
+
+  guests: Guest
+  tracks: Track
+
+  @CreateDateColumn()
+  @Field()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  @Field()
+  updatedAt: Date
+
+  @DeleteDateColumn()
+  @Field()
+  deletedAt: Date
 }
+
+
