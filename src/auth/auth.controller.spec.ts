1ba74a839e210c34c60a7299382328433d28d472
AgusBlanco <agustinanahirblanco@gmail.com>
2025-08-12 20:46:15 -0300

    feat: [MUS-35-test-login-registro-usuario]

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -3,18 +3,57 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [{
+        provide: AuthService,
+        useValue: {
+          loginService: jest.fn(),
+          registerService: jest.fn()
+        }
+      }],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService)
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  describe('loginController', () => {
+    it('Debe llamar a login service con el DTO recibido', async () => {
+      const dto: LoginAuthDto = { email: 'test@test.com', password: '123456' };
+        (authService.loginService as jest.Mock).mockResolvedValue('mock-token');
+
+      const result = await authController.loginController(dto)
+
+      expect(authService.loginService).toHaveBeenCalledWith(dto)
+      expect(result).toBe('mock-token')
+
+
+    })
+  })
+
+  describe('registerController', () => {
+    it('Si la contraseña no coincide debe lanzar un BadRequestException', async () => {
+      const dto: RegisterAuthDto = { name: 'test', lastname: 'test', email: 'test@test.com', password: '123456', repeatPassword: '1234567' };
+        (authService.registerService as jest.Mock).mockResolvedValue('user-created');
+      await expect(authController.registerController(dto))
+        .rejects.toThrow('Las contraseñas no coinciden')
+
+      expect(authService.registerService).not.toHaveBeenCalled()
+
+    })
+
+    it('Debe llamar a registerService y retornar el resultado si las contraseñas coinciden', async () => {
+      const dto: RegisterAuthDto = { name: 'test', lastname: 'test', email: 'test@test.com', password: '123456', repeatPassword: '123456' }
+        (authService.registerService as jest.Mock).mockResolvedValue('user-created')
+
+      const result = await authController.registerController(dto)
+      expect(authService.registerService).toHaveBeenCalledWith(dto)
+      expect(result).toBe('user-created')
+    })
+  })
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,110 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
+import bcrypt from 'bcrypt'
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let usersService: jest.Mocked<UsersService>
+  let jwtService: jest.Mocked<JwtService>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findUserByEmailService: jest.fn(),
+            createUserService: jest.fn()
+          }
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn()
+          }
+        }
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
+    authService = module.get<AuthService>(AuthService)
+    usersService = module.get<UsersService>(UsersService) as jest.Mocked<UsersService>
+    jwtService = module.get<JwtService>(JwtService) as jest.Mocked<JwtService>
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  describe('loginService', () => {
+    it('Debe lanzar UnathorizedException si el usuario no existe', async () => {
+      usersService.findUserByEmailService.mockResolvedValue(null)
+
+      await expect(
+        authService.loginService({
+          email: 'test@test.com',
+          password: '123456'
+        })
+      ).rejects.toThrow(UnauthorizedException)
+    })
+
+    it('Debe lanzar UnatorizedException si la contraseña es incorrecta', async () => {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      usersService.findUserByEmailService.mockResolvedValue({
+        id: '1',
+        email: 'test@test.com',
+        password: 'hashedPassword'
+      } as any);
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(false)
+
+      await expect(
+        authService.loginService({ email: "test@test.com", password: "wrong" })
+      ).rejects.toThrow(UnauthorizedException)
+    })
+
+    it("Debe devolver un token si las credenciales son correctas" async () => {
+      usersService.findUserByEmailService.mockResolvedValue({
+        id: "1",
+        email: "test@test.com",
+        password: "hashedPassword"
+      } as any)
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(true)
+      jwtService.signAsync.mockResolvedValue('fake-jwt-token')
+
+      const result = await authService.loginService({ email: 'test@test.com', password: '123456' })
+
+      expect(result).toEqual({ token: 'fake-jwt-token' })
+    })
+  })
+
+  describe('registerService', () => {
+    it('Debe lanzar UnauthorizedException si el usuario ya existe', async () => {
+      usersService.findUserByEmailService.mockResolvedValue({} as any)
+      await expect(
+        authService.registerService({
+          email: 'test@test.com',
+          password: '123456',
+          repeatPassword: '123456',
+          name: 'test'
+        } as any)
+      ).rejects.toThrow(UnauthorizedException)
+    })
+
+    it('Debe crear un usuario y devolverlo sin password', async () => {
+      usersService.findUserByEmailService.mockResolvedValue(null)
+      usersService.createUserService.mockResolvedValue({
+        id: '1',
+        email: 'test@test.com',
+        password: 'hashedPassword',
+        name: 'test'
+      } as any)
+
+      const result = await authService.registerService({
+        email: 'test@test.com',
+        password: '123456',
+        repeatPassword: '123456',
+        name: 'test'
+      } as any)
+      expect(result).not.toHaveProperty('password')
+      expect(result.email).toBe('test@test.com')
+    })
+  })
 });
