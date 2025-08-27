269fea6b2faa86a2d00ec98444c9b7f32cabc6db
Schneider Correa <schneidercorrea@gmail.com>
2025-08-26 20:12:53 -0500

    feature:desarrollo modulo storage

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { StorageModule } from 'src/storage/storage.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { GuestsModule } from './guests/guests.module';
+import { IntellectualPropertyModule } from './intellectual-property/intellectual-property.module';
 import { MusicalGenreModule } from './musical-genre/musical-genre.module';
 import { PlaylistsModule } from './playlists/playlists.module';
 import { RequestedTracksModule } from './requested-tracks/requested-tracks.module';
 import { TracksModule } from './tracks/tracks.module';
 import { UsersModule } from './users/users.module';
-import { IntellectualPropertyModule } from './intellectual-property/intellectual-property.module';
 
 @Module({
   imports: [
@@ -37,6 +38,7 @@ import { IntellectualPropertyModule } from './intellectual-property/intellectual
     RequestedTracksModule,
     MusicalGenreModule,
     IntellectualPropertyModule,
+    StorageModule.forRootAsync(),
   ],
   controllers: [],
   providers: [],
