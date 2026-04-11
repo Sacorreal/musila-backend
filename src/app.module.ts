a726d5285be29d6f96b6a7b56e2b0cdd01a45d24
Schneider Correa <schneidercorrea@gmail.com>
2026-04-11 09:58:16 -0500

    feat: implement shared infrastructure modules for storage, email, notifications, and database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from './config/config.module';
-import { DatabaseModule } from './config/database/database.module';
+import { ConfigModule } from './shared/config/config.module';
+import { DatabaseModule } from './shared/config/database/database.module';
 import { GuestsModule } from './guests/guests.module';
 import { IntellectualPropertyModule } from './intellectual-property/intellectual-property.module';
-import { LanguagesModule } from './language/languages.module';
+import { LanguagesModule } from './shared/language/languages.module';
 
 import { InvitesModule } from './invites/invites.module';
 import { MusicalGenreModule } from './musical-genre/musical-genre.module';
-import { NotificationsModule } from './notifications/notifications.module';
+import { NotificationsModule } from './shared/notifications/notifications.module';
 import { PlaylistCollaboratorsModule } from './playlist-collaborators/playlist-collaborators.module';
 import { PlaylistsModule } from './playlists/playlists.module';
 import { RequestedTracksModule } from './requested-tracks/requested-tracks.module';
 import { SearchModule } from './search/search.module';
-import { StorageModule } from './storage/storage.module';
+import { StorageModule } from './shared/storage/storage.module';
 import { TracksModule } from './tracks/tracks.module';
 import { UsersModule } from './users/users.module';
-import { EmailModule} from './mail/email.module'
+import { EmailModule} from './shared/mail/email.module'
 import { EventBusModule } from './shared/event-bus/event-bus.module';
 
 
