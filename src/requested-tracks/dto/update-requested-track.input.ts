9c7a55f778d6e32bc2ff37f901cb4437bfda89ab
Schneider Correa <schneidercorrea@gmail.com>
2026-04-27 12:20:13 -0500

    feat: add RequestsStatus enum and UpdateRequestedTrackInput DTO for track status management

--- src/requested-tracks/dto/update-requested-track.input.ts
+++ src/requested-tracks/dto/update-requested-track.input.ts
@@ -1,5 +1,16 @@
-import { RequestedTrack} from '../entities/requested-track.entity'
-import { PartialType } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { RequestsStatus } from '../entities/requests-status.enum';
+import { LicenseType } from '../entities/license-type.enum';
 
+export class UpdateRequestedTrackInput {
+  @ApiPropertyOptional({ enum: RequestsStatus })
+  @IsOptional()
+  @IsEnum(RequestsStatus)
+  status?: RequestsStatus;
 
-export class UpdateRequestedTrackInput extends PartialType(RequestedTrack) {}
+  @ApiPropertyOptional({ enum: LicenseType })
+  @IsOptional()
+  @IsEnum(LicenseType)
+  licenseType?: LicenseType;
+}
